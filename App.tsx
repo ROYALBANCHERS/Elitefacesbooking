0594a06333f91958a0988ea38c5dd15e00897850
Rishabh Kumar <126135813+Rishabhkumar023@users.noreply.github.com>
2026-02-05 15:01:45 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -180,6 +180,7 @@ const App: React.FC = () => {
                 <a href="mailto:elitefacesbooking@gmail.com" className="flex items-center justify-center md:justify-start space-x-2 hover:text-yellow-500 transition-colors">
                   <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                   <span>elitefacesbooking@gmail.com</span>
+<span>+91 9990996091</span>
                 </a>
               </li>
               <li><a href="#" className="hover:text-white transition-colors">Bollywood Talent</a></li>
